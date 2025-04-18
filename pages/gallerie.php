0fca6508386a18d2644749d72aa2fd83f8032247
yaya791 <isaiah.louisfert@efrei.net>
2025-04-18 21:06:00 +0000

    modif css +ajout div pour chaque anime pour mieux voir

--- pages/gallerie.php
+++ pages/gallerie.php
@@ -7,27 +7,32 @@ include '../inc/header.php';
 
 <article class="gallery">
   
+<div class="bloc-anime">
     <img src="<?= BASE ?>images/couverture-naruto.jpg" class="img-principale" alt="Naruto">
     <div class="miniatures">
         <img src="<?= BASE ?>images/naruto1.jpg" class="miniature" alt="Naruto 1">
         <img src="<?= BASE ?>images/naruto2.jpg" class="miniature" alt="Naruto 2">
         <img src="<?= BASE ?>images/naruto3.jpg" class="miniature" alt="Naruto 3">
     </div>
+</div>
 
-   
+<div class="bloc-anime">
     <img src="<?= BASE ?>images/couverture-one-piece.jpg" class="img-principale" alt="onepiece">
     <div class="miniatures">
         <img src="<?= BASE ?>images/onepiece1.jpg" class="miniature" alt="luffy 1">
         <img src="<?= BASE ?>images/onepiece2.jpg" class="miniature" alt="zoro 2">
         <img src="<?= BASE ?>images/onepiece3.jpg" class="miniature" alt="sanji 3">
     </div>
+</div>
 
+<div class="bloc-anime">
     <img src="<?= BASE ?>images/snk-couverture.jpeg" class="img-principale" alt="snk">
     <div class="miniatures">
         <img src="<?= BASE ?>images/snk1.jpg" class="miniature" alt="Naruto 1">
         <img src="<?= BASE ?>images/snk2.jpg" class="miniature" alt="Naruto 2">
         <img src="<?= BASE ?>images/snk3.jpg" class="miniature" alt="Naruto 3">
     </div>
+</div>
 </article>
 
 <?php include '../inc/footer.php'; ?>
